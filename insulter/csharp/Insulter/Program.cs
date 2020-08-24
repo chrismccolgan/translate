8d2a8fa23171a54888bc72b3d4b551d52bf3c96d
chrismccolgan <mccolganchrisj@gmail.com>
2020-08-24 16:01:48 -0500

    reformatted insulter

--- insulter/csharp/Insulter/Program.cs
+++ insulter/csharp/Insulter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Insulter
 {
@@ -6,23 +7,59 @@ namespace Insulter
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 3; i++)
+            List<string> insults = new List<string>
             {
-                Random rnd = new Random();
-                string[] insults = {
-                    "You look like what morning breath smells like.",
-                    "If you tried to give me cpr I would probably throw myself back under water",
-                    "I am not a fan of you",
-                    "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
-                    "I'd rather walk than be on the same plane as you",
-                    "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-                    "Do you have to be so...like that?"
-                };
+                "You look like what morning breath smells like.",
+                "If you tried to give me cpr I would probably throw myself back under water",
+                "I am not a fan of you",
+                "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+                "I'd rather walk than be on the same plane as you",
+                "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+                "Do you have to be so...like that?"
+            };
 
-                int insultsIndex = rnd.Next(insults.Length);
+            List<int> indexes = new List<int>();
+            while (indexes.Count < 3)
+            {
+                int candidate = new Random().Next(0, insults.Count - 1);
+                if (!indexes.Contains(candidate))
+                {
+                    indexes.Add(candidate);
+                }
+            }
 
-                Console.WriteLine(insults[insultsIndex]);
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                int index = indexes[i];
+                Console.WriteLine(insults[index]);
             }
         }
     }
 }
+
+// namespace Insulter
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             for (int i = 0; i < 3; i++)
+//             {
+//                 Random rnd = new Random();
+//                 string[] insults = {
+//                     "You look like what morning breath smells like.",
+//                     "If you tried to give me cpr I would probably throw myself back under water",
+//                     "I am not a fan of you",
+//                     "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+//                     "I'd rather walk than be on the same plane as you",
+//                     "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+//                     "Do you have to be so...like that?"
+//                 };
+
+//                 int insultsIndex = rnd.Next(insults.Length);
+
+//                 Console.WriteLine(insults[insultsIndex]);
+//             }
+//         }
+//     }
+// }
