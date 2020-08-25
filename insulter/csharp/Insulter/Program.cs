7affeda48bdd8abdb6624a2e4bbec722668327f8
chrismccolgan <mccolganchrisj@gmail.com>
2020-08-25 10:00:29 -0500

    cleaned up insulter

--- insulter/csharp/Insulter/Program.cs
+++ insulter/csharp/Insulter/Program.cs
@@ -7,7 +7,7 @@ namespace Insulter
     {
         static void Main(string[] args)
         {
-            List<string> insults = new List<string>
+            List<string> insults = new List<string>()
             {
                 "You look like what morning breath smells like.",
                 "If you tried to give me cpr I would probably throw myself back under water",
@@ -21,7 +21,7 @@ namespace Insulter
             List<int> indexes = new List<int>();
             while (indexes.Count < 3)
             {
-                int candidate = new Random().Next(0, insults.Count - 1);
+                int candidate = new Random().Next(0, insults.Count);
                 if (!indexes.Contains(candidate))
                 {
                     indexes.Add(candidate);
@@ -36,30 +36,3 @@ namespace Insulter
         }
     }
 }
-
-// namespace Insulter
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             for (int i = 0; i < 3; i++)
-//             {
-//                 Random rnd = new Random();
-//                 string[] insults = {
-//                     "You look like what morning breath smells like.",
-//                     "If you tried to give me cpr I would probably throw myself back under water",
-//                     "I am not a fan of you",
-//                     "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
-//                     "I'd rather walk than be on the same plane as you",
-//                     "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-//                     "Do you have to be so...like that?"
-//                 };
-
-//                 int insultsIndex = rnd.Next(insults.Length);
-
-//                 Console.WriteLine(insults[insultsIndex]);
-//             }
-//         }
-//     }
-// }
